20a02cd0bb6f2dacc3cdcaa7ef2f7bb517fc6b88
Huỳnh Minh Khang <91047241+akagiyuu@users.noreply.github.com>
2025-03-23 01:18:38 +0700

    Update main.rs

--- chapter-fetcher/src/main.rs
+++ chapter-fetcher/src/main.rs
@@ -7,7 +7,7 @@ use serde::Deserialize;
 use serde_json::Value;
 use tokio::fs;
 
-const BASE_URL: &str = "https://cmangax.com";
+const BASE_URL: &str = "https://cmangaax.com";
 
 #[derive(Debug, Deserialize)]
 struct ComicInfo {
