2b2adba9c9b816c61b9e767f38066143da814e04
akagiyuu <khanghuynh0245@gmail.com>
2025-04-02 12:12:14 +0700

    feat: init tracing

--- comic-fake-view/src-tauri/src/lib.rs
+++ comic-fake-view/src-tauri/src/lib.rs
@@ -1,12 +1,37 @@
+use std::fs::File;
+
+use tracing::level_filters::LevelFilter;
+use tracing_subscriber::{fmt, layer::SubscriberExt, util::SubscriberInitExt, EnvFilter};
+
 pub mod command;
 pub mod config;
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
+    let log_file = File::create("tracing.log").unwrap();
+
+    tracing_subscriber::registry()
+        .with(
+            tracing_subscriber::fmt::layer()
+                .pretty()
+                .with_timer(fmt::time::ChronoLocal::rfc_3339()),
+        )
+        .with(tracing_subscriber::fmt::layer().with_writer(log_file))
+        .with(
+            EnvFilter::builder()
+                .with_default_directive(LevelFilter::INFO.into())
+                .from_env_lossy(),
+        )
+        .init();
+
     tauri::Builder::default()
         .plugin(tauri_plugin_dialog::init())
         .plugin(tauri_plugin_opener::init())
-        .invoke_handler(tauri::generate_handler![command::set_config, command::get_config, command::run])
+        .invoke_handler(tauri::generate_handler![
+            command::set_config,
+            command::get_config,
+            command::run
+        ])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
