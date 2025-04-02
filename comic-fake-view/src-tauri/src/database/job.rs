4b312caabecf90914e7c4a8d7c2883ddf5ef37ca
akagiyuu <khanghuynh0245@gmail.com>
2025-04-02 13:29:52 +0700

    feat: return receiver directly

--- comic-fake-view/src-tauri/src/database/job.rs
+++ comic-fake-view/src-tauri/src/database/job.rs
@@ -12,11 +12,18 @@ pub async fn is_empty(pool: &SqlitePool) -> Result<bool> {
     count(pool).await.map(|count| count == 0)
 }
 
-pub async fn all(pool: &SqlitePool) -> Result<Vec<String>> {
+pub async fn all(pool: &SqlitePool) -> Result<flume::Receiver<String>> {
     let jobs = sqlx::query_scalar("SELECT url FROM jobs WHERE is_read = false")
         .fetch_all(pool)
         .await?;
-    Ok(jobs)
+
+    let (sender, receiver) = flume::unbounded::<String>();
+
+    for job in jobs {
+        sender.send_async(job).await?;
+    }
+
+    Ok(receiver)
 }
 
 pub async fn done(url: &str, pool: &SqlitePool) -> Result<()> {
