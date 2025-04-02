fdb3b4b13c5208350952290a078a911641889252
akagiyuu <khanghuynh0245@gmail.com>
2025-04-02 11:10:13 +0700

    feat: save config

--- comic-fake-view/src-tauri/src/command/set_config.rs
+++ comic-fake-view/src-tauri/src/command/set_config.rs
@@ -1,8 +1,6 @@
 use crate::config::Config;
-use futures::lock::Mutex;
-use tauri::Manager;
 
 #[tauri::command]
 pub async fn set_config(app_handle: tauri::AppHandle, config: Config) {
-    *app_handle.state::<Mutex<Config>>().lock().await = config;
+    config.save().await;
 }
