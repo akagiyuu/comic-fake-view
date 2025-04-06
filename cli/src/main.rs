e1ee029fb2e8175c5a45f34867e37c193c8b33e7
akagiyuu <khanghuynh0245@gmail.com>
2025-04-06 15:17:39 +0700

    feat: add progress bar

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -1,5 +1,8 @@
+use std::sync::Arc;
+
 use anyhow::Result;
 use comic_fake_view_core::config::Config;
+use indicatif::ProgressBar;
 use tokio::sync::{mpsc, watch};
 use tracing::level_filters::LevelFilter;
 use tracing_subscriber::{
@@ -24,20 +27,29 @@ async fn main() -> Result<()> {
         )
         .init();
 
-    let config = Config::load();
+    let config = Arc::new(Config::load());
     let (_cancellation_sender, cancellation) = watch::channel(false);
 
-    let (progress_notifier, _progress_receiver) = mpsc::unbounded_channel();
+    let (progress_notifier, mut progress_receiver) =
+        mpsc::unbounded_channel::<(&'static str, String)>();
+    let automation_handler = tokio::spawn(async move {
+        comic_fake_view_core::run(progress_notifier, cancellation, config.as_ref()).await
+    });
 
-    // tokio::spawn(async move {
-    //     while let Some((event, data)) = progress_receiver.recv().await {
-    //         match event {
-    //             _ => {}
-    //         }
-    //     }
-    // });
+    let (_, job_count) = progress_receiver.recv().await.unwrap();
+    let bar = ProgressBar::new(job_count.parse().unwrap());
 
-    comic_fake_view_core::run(progress_notifier, cancellation, &config).await?;
+    let progress_handler = tokio::spawn(async move {
+        while let Some((event, _)) = progress_receiver.recv().await {
+            if let "complete" = event {
+                bar.inc(1);
+            }
+        }
+
+        Ok::<(), anyhow::Error>(())
+    });
+
+    let _ = tokio::join!(automation_handler, progress_handler);
 
     Ok(())
 }
